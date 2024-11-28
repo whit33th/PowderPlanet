3c23746a62ca8fad8829f9a56ab44af68594d77d
Daniil Kaminskyi <98901710+whit33th@users.noreply.github.com>
2024-11-28 01:27:51 +0100

    chom-chom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <head><link rel="icon" href="/favicon.ico" sizes="any" /></head>
       
       <body
-        className={`${league} antialiased`}
+        className={`antialiased`}
       >
         {children} 
       </body>
