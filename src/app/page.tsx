3e191d31e397481c750d599e188eb886327ab453
Daniil Kaminskyi <98901710+whit33th@users.noreply.github.com>
2024-11-28 20:59:18 +0100

    oki-doki

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import HomePage from '@/components/Containers/HomePage'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Info from '@/components/Containers/Info'
 import JinxShortInfo from '@/components/Containers/JinxShortInfo'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Skills from '@/components/Containers/Skills'
 import ConceptArt from './../components/Containers/ConceptArt'
 
